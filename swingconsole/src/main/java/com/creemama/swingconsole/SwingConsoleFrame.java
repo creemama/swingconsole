9cbacb99914372e9f7c48c722deaa23c3e52b279
Chris Topher <c@creemama.com>
2020-03-09 18:54:36 -0400

    refactor: Move common code from SwingConsoleDialog and SwingConsoleFrame into SwingConsoleWindow

--- swingconsole/src/main/java/com/creemama/swingconsole/SwingConsoleFrame.java
+++ swingconsole/src/main/java/com/creemama/swingconsole/SwingConsoleFrame.java
@@ -1,121 +1,37 @@
 package com.creemama.swingconsole;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Insets;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-
-import javax.swing.BorderFactory;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.SwingUtilities;
 
 /**
  * A {@link JFrame} that displays an interactive console with possible readline
  * functionality like tab completion and command history.
  */
 public class SwingConsoleFrame extends JFrame {
-	private static final long serialVersionUID = 3746242973444417387L;
+	final static long serialVersionUID = 3746242973444417387L;
 
-	private boolean running;
-
-	private TextAreaReadline tar;
+	final private SwingConsoleWindow common;
 
 	public SwingConsoleFrame(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.common = new SwingConsoleWindow();
 	}
 
 	@Override
 	public void dispose() {
-		if (SwingUtilities.isEventDispatchThread())
-			disposeOnAWTEDT();
-		else
-			SwingUtilities.invokeLater(this::disposeOnAWTEDT);
-	}
-
-	private void disposeOnAWTEDT() {
-		// Since tar.shutdown could take a few seconds, let us hide the window if it is
-		// not already hidden.
-		setVisible(false);
-		if (tar != null) {
-			tar.shutdown();
-			tar = null;
-		}
-		getContentPane().removeAll();
-		super.dispose();
-		running = false;
+		common.dispose(getContentPane(), () -> super.dispose(), this);
 	}
 
 	public boolean isRunning() {
-		return running;
+		return common.isRunning();
 	}
 
 	public void run(String[] args, SwingConsoleModel model) {
-		if (SwingUtilities.isEventDispatchThread())
-			runOnAWTEDT(args, model);
-		else
-			SwingUtilities.invokeLater(() -> runOnAWTEDT(args, model));
-	}
-
-	private void runOnAWTEDT(String[] args, SwingConsoleModel model) {
-		// NOTE: If you update this method, also update SwingConsoleDialog#run.
-
-		if (running)
-			throw new IllegalStateException(
-					"You already called #run. Only call #run again after disposing of this window.");
-		running = true;
-
-		JEditorPane text = new JTextPane();
-		text.setBackground(new Color(0xf2, 0xf2, 0xf2));
-		text.setCaretColor(new Color(0xa4, 0x00, 0x00));
-		Font font = SwingConsoleUtil.findFont("Monospaced", Font.PLAIN, 14, new String[] { "Monaco", "Andale Mono" });
-		text.setFont(font);
-		text.setForeground(new Color(0xa4, 0x00, 0x00));
-		text.setMargin(new Insets(8, 8, 8, 8));
-
-		tar = new TextAreaReadline(text, " Welcome to the " + getTitle() + " \n\n");
-
-		JScrollPane pane = new JScrollPane();
-		pane.setBorder(BorderFactory.createLineBorder(Color.darkGray));
-		pane.setViewportView(text);
-
-		setSize(700, 600);
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(pane, BorderLayout.CENTER);
-
-		Thread swingConsoleThread = new Thread(() -> {
-			model.setUp(Arrays.asList(args), tar);
-			try {
-				SwingUtilities.invokeAndWait(() -> setVisible(true));
-			} catch (InvocationTargetException | InterruptedException e) {
-				// Do nothing.
-			}
-			try {
-				model.run(tar);
-			} finally {
-				SwingUtilities.invokeLater(() -> dispose());
-			}
-		}, getTitle());
-		swingConsoleThread.setDaemon(true);
-		swingConsoleThread.start();
+		common.run(args, getContentPane(), model, getTitle(), this);
 	}
 
 	@Override
 	public void setVisible(boolean visible) {
-		if (SwingUtilities.isEventDispatchThread())
-			setVisibleOnAWTEDT(visible);
-		else
-			SwingUtilities.invokeLater(() -> setVisibleOnAWTEDT(visible));
-	}
-
-	private void setVisibleOnAWTEDT(boolean visible) {
-		if (visible && !running)
-			throw new IllegalStateException("Call #run first.");
-		super.setVisible(visible);
+		common.setVisible(v -> super.setVisible(v), visible);
 	}
 }
---
+++ swingconsole/src/main/java/com/creemama/swingconsole/SwingConsoleWindow.java
@@ -0,0 +1,111 @@
+package com.creemama.swingconsole;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.Window;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+import javax.swing.BorderFactory;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+
+/**
+ * Common code between {@link SwingConsoleDialog} and {@link SwingConsoleFrame}.
+ */
+class SwingConsoleWindow {
+	private boolean running;
+
+	private TextAreaReadline tar;
+
+	void dispose(Container container, Runnable superDispose, Window window) {
+		if (SwingUtilities.isEventDispatchThread())
+			disposeOnAWTEDT(container, superDispose, window);
+		else
+			SwingUtilities.invokeLater(() -> disposeOnAWTEDT(container, superDispose, window));
+	}
+
+	private void disposeOnAWTEDT(Container container, Runnable superDispose, Window window) {
+		// Since tar.shutdown could take a few seconds, let us hide the window if it is
+		// not already hidden.
+		window.setVisible(false);
+		if (tar != null) {
+			tar.shutdown();
+			tar = null;
+		}
+		container.removeAll();
+		superDispose.run();
+		running = false;
+	}
+
+	boolean isRunning() {
+		return running;
+	}
+
+	void run(String[] args, Container container, SwingConsoleModel model, String title, Window window) {
+		if (SwingUtilities.isEventDispatchThread())
+			runOnAWTEDT(args, container, model, title, window);
+		else
+			SwingUtilities.invokeLater(() -> runOnAWTEDT(args, container, model, title, window));
+	}
+
+	private void runOnAWTEDT(String[] args, Container container, SwingConsoleModel model, String title, Window window) {
+		if (running)
+			throw new IllegalStateException(
+					"You already called #run. Only call #run again after disposing of this window.");
+		running = true;
+
+		JEditorPane text = new JTextPane();
+		text.setBackground(new Color(0xf2, 0xf2, 0xf2));
+		text.setCaretColor(new Color(0xa4, 0x00, 0x00));
+		Font font = SwingConsoleUtil.findFont("Monospaced", Font.PLAIN, 14, new String[] { "Monaco", "Andale Mono" });
+		text.setFont(font);
+		text.setForeground(new Color(0xa4, 0x00, 0x00));
+		text.setMargin(new Insets(8, 8, 8, 8));
+
+		tar = new TextAreaReadline(text, " Welcome to the " + title + " \n\n");
+
+		JScrollPane pane = new JScrollPane();
+		pane.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+		pane.setViewportView(text);
+
+		window.setSize(700, 600);
+		container.setLayout(new BorderLayout());
+		container.add(pane, BorderLayout.CENTER);
+
+		Thread swingConsoleThread = new Thread(() -> {
+			model.setUp(Arrays.asList(args), tar);
+			try {
+				SwingUtilities.invokeAndWait(() -> window.setVisible(true));
+			} catch (InvocationTargetException | InterruptedException e) {
+				// Do nothing.
+			}
+			try {
+				model.run(tar);
+			} finally {
+				SwingUtilities.invokeLater(() -> window.dispose());
+			}
+		}, title);
+		swingConsoleThread.setDaemon(true);
+		swingConsoleThread.start();
+	}
+
+	void setVisible(Consumer<Boolean> superSetVisible, boolean visible) {
+		if (SwingUtilities.isEventDispatchThread())
+			setVisibleOnAWTEDT(superSetVisible, visible);
+		else
+			SwingUtilities.invokeLater(() -> setVisibleOnAWTEDT(superSetVisible, visible));
+	}
+
+	private void setVisibleOnAWTEDT(Consumer<Boolean> superSetVisible, boolean visible) {
+		if (visible && !running)
+			throw new IllegalStateException("Call #run first.");
+		superSetVisible.accept(visible);
+	}
+}
